38de7ec4d08c68157153d7f83231d5947fa40c7a
Veldanov <aveldanov@MTVL16074381b.local>
2018-11-15 20:12:20 -0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//Using Third Party component
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css'
+import './styles/style.scss';
+
+
+ReactDOM.render(<p>This is my page</p>, document.getElementById("app"));
+
